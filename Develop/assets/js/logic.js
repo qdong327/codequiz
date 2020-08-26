dc5ec5f24522a67abfbce12b93f19a93e1f627ea
Qian Dong <qdong327@gmail.com>
2020-08-26 00:33:27 -0400

    working timer

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -1,6 +1,7 @@
 // variables to keep track of quiz state (from instructor)
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var startingTime = questions.length * 15;
+var currentTime = startingTime;
 var timerId;
 
 // variables to reference DOM elements (from instructor)
@@ -18,44 +19,54 @@ var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
 function startQuiz() {
   // hide start screen (from instructor)
- 
+
   // un-hide questions section (from instructor)
 
   // start timer (from instructor)
-  var setTimeout(startTimer() {
-    console.log("done")},60000);
+  timerId = setInterval(function () {
+    var secondsLeft = currentTime--;
+    timerEl.textContent = (secondsLeft + " seconds");
+    console.log(secondsLeft);
+
+    // QD: clearing interval below currently does not work, numbers hit negative
+    // if (secondsLeft < 1)
+    //   clearInterval();
+    //   secondsLeft = 0;
+  }, 1000);
+
 
   // show starting time (from instructor)
 
   getQuestion();
 }
 
+
 function getQuestion() {
   // get current question object from array (from instructor)
-questions[currentQuestionIndex];
+  questions[currentQuestionIndex];
   // update title with current question (from instructor)
 
   // clear out any old question choices (from instructor)
 
   // loop over choices (from instructor)
 
-    // create new button for each choice (from instructor)
+  // create new button for each choice (from instructor)
 
-    // attach click event listener to each choice (from instructor)
+  // attach click event listener to each choice (from instructor)
 
-    // display on the page (from instructor)
+  // display on the page (from instructor)
 }
 
 function questionClick() {
   // check if user guessed wrong (from instructor)
-    // penalize time (from instructor)
+  // penalize time (from instructor)
 
-    // display new time on page (from instructor)
+  // display new time on page (from instructor)
 
-    // play "wrong" sound effect (from instructor)
+  // play "wrong" sound effect (from instructor)
 
   // else  (from instructor)
-    // play "right" sound effect (from instructor)
+  // play "right" sound effect (from instructor)
 
 
   // flash right/wrong feedback on page for half a second (from instructor)
@@ -63,19 +74,23 @@ function questionClick() {
   // move to next question (from instructor)
 
   // check if we've run out of questions (from instructor)
-    // quizEnd (from instructor)
+  // quizEnd (from instructor)
   // else  (from instructor)
-    // getQuestion (from instructor)
+  // getQuestion (from instructor)
 }
 
 function quizEnd() {
   // stop timer (from instructor)
 
+  // clearInterval();
+
   // show end screen (from instructor)
 
   // show final score (from instructor)
-
+  finalScoreEl.textContent = userScore;
+  console.log(finalScoreEl);
   // hide questions section (from instructor)
+  questionsEl.className = "hide";
 }
 
 function clockTick() {
@@ -88,25 +103,24 @@ function saveHighscore() {
   // get value of input box (from instructor)
 
   // make sure value wasn't empty (from instructor)
-    // get saved scores from localstorage, or if not any, set to empty array (from instructor)
+  // get saved scores from localstorage, or if not any, set to empty array (from instructor)
 
-    // format new score object for current user (from instructor)
+  // format new score object for current user (from instructor)
 
-    // save to localstorage (from instructor)
+  // save to localstorage (from instructor)
 
-    // redirect to next page (from instructor)
+  // redirect to next page (from instructor)
 }
 
 function checkForEnter(event) {
   // check if event key is enter (from instructor)
-    // saveHighscore (from instructor)
+  // saveHighscore (from instructor)
 }
 
 // user clicks button to submit initials (from instructor)
-submitBtn.onclick = saveHighscore;
+// submitBtn.onclick = saveHighscore;
 
 // user clicks button to start quiz (from instructor)
-startBtn.onclick = startQuiz;
-startBtn.onclick = startTimer;
+startBtn.addEventListener('click', startQuiz);
 
-initialsEl.onkeyup = checkForEnter;
+// initialsEl.onkeyup = checkForEnter;
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<!-- Trivia Game -->
+Trivia Game
 
 Using Javascript, HTML, and CSS, this is a code quiz using timers, scoring, and special effects.
-
