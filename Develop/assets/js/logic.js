53eb91d8caa537c28206d0c84432dcb943c40fba
Qian Dong <qdong327@gmail.com>
2020-08-29 09:06:21 -0400

    figured out remaining logic, onto high scores, fixed formatting issue

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -3,9 +3,10 @@
 // variables to keep track of quiz state (from instructor)
 var currentQuestionIndex = 0;
 var startingTime = questions.length * 15;
-
+var currentQuestion = questions[currentQuestionIndex];
 //Test Timer
 // var startingTime = 3;
+
 var currentTime = startingTime;
 var timerId;
 
@@ -21,21 +22,13 @@ var startingEl = document.querySelector("#start-screen");
 var questionTitle = document.querySelector("#question-title");
 var questionChoices = document.querySelector("#choices");
 var wrapper = document.querySelector(".wrapper");
+var finalScoreEl = document.querySelector("#final-score");
+// var endingElement = document.querySelector("#end-screen");
 
 // sound effects (from instructor)
 var sfxRight = new Audio("assets/sfx/correct.wav");
 var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
-// checking if jquery is loaded
-// window.onload = function() {
-//   if (window.jQuery) {  
-//       // jQuery is loaded  
-//       alert("Yeah!");
-//   } else {
-//       // jQuery is not loaded
-//       alert("Doesn't Work");
-//   }
-// }
 $(document).ready(function () { console.log("jQuery ready") });
 
 //----- Global Functions -----//
@@ -66,7 +59,7 @@ function hideStart() {
 //Function Definition: Show the Questions
 function showQuestions() {
   $("#questions").show();
-  getQuestion();
+  displayQuestion();
 }
 // Function Definition: Start the Quiz
 function startQuiz() {
@@ -83,13 +76,12 @@ function penalizeTime() {
   timerEl.innerHTML = timerEl.innerHTML - 10
   // if time, add in flash of red background color when penalized
 }
-function getQuestion() {
+function displayQuestion() {
   // get current question object from array (from instructor)
-  var currentQuestion = questions[currentQuestionIndex];
   // update title with current question (from instructor)
   questionTitle.innerHTML = currentQuestion.title;
   // clear out any old question choices (from instructor)
-  // currentQuestion[i-1].choices.classList.add("hide");
+  choicesEl.innerHTML = "";
   // loop over choices (from instructor)
   for (var i = 0; i < currentQuestion.choices.length; i++) {
 
@@ -97,58 +89,74 @@ function getQuestion() {
     var newButton = document.createElement("button");
     newButton.id = i;
     newButton.innerHTML = +[i] + 1 + ": " + currentQuestion.choices[i];
-    newButton.className = "choices";
+    newButton.className = "choices button";
     // display on the page (from instructor)
-    wrapper.appendChild(newButton);
+    choicesEl.appendChild(newButton);
     newButton.onclick = checkAnswer;
     // console.log(questions[currentQuestionIndex].answer);
   }
+}
+// More function definitions
+function removeFeedback() {
+  feedbackEl.classList.add("hide");
+}
 
-  function checkAnswer() {
-    // console.log(this.innerHTML[0]-1);
-    // console.log(currentQuestion.choices[parseInt(userSelection)])
-    var userSelection = this.innerHTML[0] - 1;
-    if (currentQuestion.choices[parseInt(userSelection)] == currentQuestion.answer) {
-      //   // play "right" sound effect (from instructor)
-      sfxRight.play();
-      $("#correct").show();
-    }
-    //   // else  (from instructor)
-    else {
-      //   // penalize time (from instructor)
-      penalizeTime();
-      //   // play "wrong" sound effect (from instructor)
-      sfxWrong.play();
-      $("#incorrect").show();
-    }
+function checkAnswer() {
+  // console.log(this.innerHTML[0]-1);
+  // console.log(currentQuestion.choices[parseInt(userSelection)])
+  var userSelection = this.innerHTML[0] - 1;
+  if (currentQuestion.choices[parseInt(userSelection)] == currentQuestion.answer) {
+    //   // play "right" sound effect (from instructor)
+    sfxRight.play();
+    feedbackEl.innerHTML = "Correct!"
+    feedbackEl.classList.remove("hide");
+    setTimeout(removeFeedback, 750)
+    getNextQuestion();
+  }
+  //   // else  (from instructor)
+  else {
+    //   // penalize time (from instructor)
+    penalizeTime();
+    //   // play "wrong" sound effect (from instructor)
+    sfxWrong.play();
+    feedbackEl.innerHTML = "Nope!"
+    feedbackEl.classList.remove("hide");
+    setTimeout(removeFeedback, 750)
   }
 }
 
-
-  // flash right/wrong feedback on page for half a second (from instructor)
-
 //   // move to next question (from instructor)
-
-//   // check if we've run out of questions (from instructor)
-//   // quizEnd (from instructor)
-//   // else  (from instructor)
-//   // getQuestion (from instructor)
-//   currentQuestionIndex++;
-//   getQuestion();
-// }
+function getNextQuestion() {
+  //   // check if we've run out of questions (from instructor)
+  // [q1, q2, q3, q4, q5] ex: current index is 4
+  if (currentQuestionIndex == questions.length - 1) {
+    //   // quizEnd (from instructor)
+    quizEnd();
+  }
+  //   // else  (from instructor)
+  else {
+    //   // displayQuestion (from instructor)
+    currentQuestionIndex = currentQuestionIndex + 1;
+    currentQuestion = questions[currentQuestionIndex];
+    console.log(currentQuestion);
+    displayQuestion();
+  }
+}
 
 function quizEnd() {
+  console.log("quizEnd");
   // stop timer (from instructor)
-
+  stopTimer(timerId);
   // clearInterval();
 
   // show end screen (from instructor)
-
+  $("#end-screen").show();
   // show final score (from instructor)
-  finalScoreEl.textContent = userScore;
+  finalScoreEl.innerHTML = timerEl.innerHTML;
   console.log(finalScoreEl);
   // hide questions section (from instructor)
-  questionsEl.className = "hide";
+  // questionsEl.className = "hide";
+  $("#questions").hide();
 }
 
 function clockTick() {
@@ -159,15 +167,23 @@ function clockTick() {
 
 function saveHighscore() {
   // get value of input box (from instructor)
-
+  var userInitials = $("#initials").val();
+  var userScore = finalScoreEl.innerHTML;
   // make sure value wasn't empty (from instructor)
+  console.log(userInitials);
+  console.log(userScore);
+
   // get saved scores from localstorage, or if not any, set to empty array (from instructor)
+var scoreArray = [];
+  // localStorage.getItem();
+  // localStorage.setItem(userInitials, "something")
 
   // format new score object for current user (from instructor)
 
   // save to localstorage (from instructor)
 
   // redirect to next page (from instructor)
+
 }
 
 function checkForEnter(event) {
