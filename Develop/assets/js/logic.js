8bd5836e844d5d60296b0c7dd3c9324dac0d7b22
Qian Dong <qdong327@gmail.com>
2020-08-29 16:37:12 -0400

    removed comments and scrubbed code

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -4,9 +4,6 @@
 var currentQuestionIndex = 0;
 var startingTime = questions.length * 15;
 var currentQuestion = questions[currentQuestionIndex];
-//Test Timer
-// var startingTime = 3;
-
 var currentTime = startingTime;
 var timerId;
 
@@ -23,7 +20,6 @@ var questionTitle = document.querySelector("#question-title");
 var questionChoices = document.querySelector("#choices");
 var wrapper = document.querySelector(".wrapper");
 var finalScoreEl = document.querySelector("#final-score");
-// var endingElement = document.querySelector("#end-screen");
 
 // sound effects (from instructor)
 var sfxRight = new Audio("assets/sfx/correct.wav");
@@ -46,17 +42,14 @@ function createTimer() {
     }
   }, 1000);
 }
-
 //Function Definition: Stop the Timer
 function stopTimer(x) {
   clearInterval(x);
 }
-
 //Function Definition: Hide the Starting prompt
 function hideStart() {
   $("#start-screen").hide();
 }
-
 //Function Definition: Show the Questions
 function showQuestions() {
   $("#questions").show();
@@ -82,6 +75,7 @@ function penalizeTime() {
   }
   // if time, add in flash of red background color when penalized
 }
+// Display Question
 function displayQuestion() {
   // get current question object from array (from instructor)
   // update title with current question (from instructor)
@@ -99,17 +93,14 @@ function displayQuestion() {
     // display on the page (from instructor)
     choicesEl.appendChild(newButton);
     newButton.onclick = checkAnswer;
-    // console.log(questions[currentQuestionIndex].answer);
   }
 }
-// More function definitions
+// Remove Feedback
 function removeFeedback() {
   feedbackEl.classList.add("hide");
 }
-
+// Check Answer
 function checkAnswer() {
-  // console.log(this.innerHTML[0]-1);
-  // console.log(currentQuestion.choices[parseInt(userSelection)])
   var userSelection = this.innerHTML[0] - 1;
   if (currentQuestion.choices[parseInt(userSelection)] == currentQuestion.answer) {
     //   // play "right" sound effect (from instructor)
@@ -130,67 +121,44 @@ function checkAnswer() {
     setTimeout(removeFeedback, 750)
   }
 }
-
-//   // move to next question (from instructor)
+// move to next question (from instructor)
 function getNextQuestion() {
-  //   // check if we've run out of questions (from instructor)
-  // [q1, q2, q3, q4, q5] ex: current index is 4
+  // check if we've run out of questions (from instructor)
   if (currentQuestionIndex == questions.length - 1) {
-    //   // quizEnd (from instructor)
+    // quizEnd (from instructor)
     quizEnd();
   }
-  //   // else  (from instructor)
   else {
-    //   // displayQuestion (from instructor)
+    // displayQuestion (from instructor)
     currentQuestionIndex = currentQuestionIndex + 1;
     currentQuestion = questions[currentQuestionIndex];
-    console.log(currentQuestion);
     displayQuestion();
   }
 }
-
 function quizEnd() {
-  console.log("quizEnd");
   // stop timer (from instructor)
   stopTimer(timerId);
-  // clearInterval();
-
   // show end screen (from instructor)
   $("#end-screen").show();
   // show final score (from instructor)
   finalScoreEl.innerHTML = timerEl.innerHTML;
-  console.log(finalScoreEl);
   // hide questions section (from instructor)
-  // questionsEl.className = "hide";
   $("#questions").hide();
 }
-
-function clockTick() {
-  // update time (from instructor)
-
-
-}
-
 function saveHighscore() {
   // get value of input box (from instructor)
   var userInitials = $("#initials").val();
   var userScore = finalScoreEl.innerHTML;
-  // make sure value wasn't empty (from instructor)
-  console.log(userInitials);
-  console.log(userScore);
-
   // get saved scores from localstorage, or if not any, set to empty array (from instructor)
   var addToScoreBoard;
-  //if highscores exists in local storage, get it from local storage
+  // make sure value wasn't empty (from instructor)
   if (userInitials !== "") {
+    //if highscores exists in local storage, get it from local storage
     var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
     addToScoreBoard = {
       score: userScore,
       initials: userInitials
     };
-    console.log(addToScoreBoard);
-    // $(#highscores)
-    // localStorage.getItem();
     highScores.push(addToScoreBoard);
     highScores = window.localStorage.setItem("highScores", JSON.stringify(highScores));
   }
@@ -202,17 +170,7 @@ function saveHighscore() {
   window.location.href = "highscores.html";
 }
 
-// function checkForEnter(e) {
-//   // check if event key is enter (from instructor)
-//   // saveHighscore (from instructor)
-//   saveHighscore();
-// }
-
 // user clicks button to submit initials (from instructor)
 submitBtn.onclick = saveHighscore;
-
 // user clicks button to start quiz (from instructor)
 startBtn.addEventListener('click', startQuiz);
-
-// check for enter call
-// initialsEl.onkeydown = checkForEnter;
--- Develop/assets/js/scores.js
+++ Develop/assets/js/scores.js
@@ -1,41 +1,17 @@
 var clearButton = document.querySelector("#clear");
-
+// Print High Scores based on local storage
 function printHighscores() {
   // either get scores from localstorage or set to empty array (from instructor)
   var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
   // (optional) sort highscores by score property in descending order (from instructor)
-
-  // Testing array = [a, b, c, d, e, f, g]
-  // var highScores = [
-  //  {
-  //     score: 12,
-  //     initials: "test"
-  //   }, {
-  //     score: 8,
-  //     initials: "test"
-  //   }, {
-  //     score: 100,
-  //     initials: "test"
-  //   }, {
-  //     score: 2,
-  //     initials: "test"
-  //   }, {
-  //     score: 50,
-  //     initials: "test"
-  //   },
-  // ]
   highScores.sort(function (a, b) {
     return b.score - a.score;
   }
   );
-  console.log(highScores)
-
   // for each score (from instructor)
-
   for (i = 0; i < highScores.length; i++) {
     var gottenInitials = highScores[i].initials;
     var gottenScore = highScores[i].score;
-    console.log(liEl);
     // create li tag for each high score (from instructor)
     var liEl = document.createElement("li")
     liEl.innerHTML = (gottenInitials + " - " + gottenScore);
@@ -43,19 +19,16 @@ function printHighscores() {
     var olEl = document.getElementById("highscores");
     olEl.append(liEl);
   }
-
 }
-
+// Clear high scores when button is clicked
 function clearHighscores() {
-  // (and reload) (from instructor)
-  console.log(clearButton);
   window.localStorage.clear();
+  // (and reload) (from instructor)
   window.location.href = "highscores.html";
 }
-clearButton.onclick = clearHighscores;
-
 
 // attache clear event to clear score button (from instructor)
+clearButton.onclick = clearHighscores;
 
 // run printhighscore when page loads (from instructor)
 printHighscores();
