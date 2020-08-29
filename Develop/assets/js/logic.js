d7d46b27ea6b68207f5ab2d87ea8e0c4f269a997
Qian Dong <qdong327@gmail.com>
2020-08-28 22:29:20 -0400

    fixed getQuestion, added checkAnswer, fixed wrapper condition

--- Develop/assets/js/logic.js
+++ Develop/assets/js/logic.js
@@ -1,10 +1,15 @@
+//----- Global Variables -----//
+
 // variables to keep track of quiz state (from instructor)
 var currentQuestionIndex = 0;
 var startingTime = questions.length * 15;
+
+//Test Timer
+// var startingTime = 3;
 var currentTime = startingTime;
 var timerId;
 
-// variables to reference DOM elements (from instructor)
+// variables to reference DOM elements (from instructor until feedbackEl)
 var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
@@ -12,72 +17,125 @@ var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+var startingEl = document.querySelector("#start-screen");
+var questionTitle = document.querySelector("#question-title");
+var questionChoices = document.querySelector("#choices");
+var wrapper = document.querySelector(".wrapper");
 
 // sound effects (from instructor)
 var sfxRight = new Audio("assets/sfx/correct.wav");
 var sfxWrong = new Audio("assets/sfx/incorrect.wav");
 
-function startQuiz() {
-  // hide start screen (from instructor)
+// checking if jquery is loaded
+// window.onload = function() {
+//   if (window.jQuery) {  
+//       // jQuery is loaded  
+//       alert("Yeah!");
+//   } else {
+//       // jQuery is not loaded
+//       alert("Doesn't Work");
+//   }
+// }
+$(document).ready(function () { console.log("jQuery ready") });
 
-  // un-hide questions section (from instructor)
+//----- Global Functions -----//
 
-  // start timer (from instructor)
+//Function Definition: Create the Timer
+function createTimer() {
+  timerEl.innerHTML = startingTime;
+
   timerId = setInterval(function () {
-    var secondsLeft = currentTime--;
-    timerEl.textContent = (secondsLeft + " seconds");
-    console.log(secondsLeft);
+    timerEl.innerHTML = +timerEl.innerHTML - 1;
 
-    // QD: clearing interval below currently does not work, numbers hit negative
-    // if (secondsLeft < 1)
-    //   clearInterval();
-    //   secondsLeft = 0;
+    if (timerEl.innerHTML < 1) {
+      stopTimer(timerId);
+    }
   }, 1000);
+}
 
+//Function Definition: Stop the Timer
+function stopTimer(x) {
+  clearInterval(x);
+}
 
-  // show starting time (from instructor)
+//Function Definition: Hide the Starting prompt
+function hideStart() {
+  $("#start-screen").hide();
+}
 
+//Function Definition: Show the Questions
+function showQuestions() {
+  $("#questions").show();
   getQuestion();
 }
-
-
+// Function Definition: Start the Quiz
+function startQuiz() {
+  // hide start screen (from instructor)
+  hideStart();
+  // un-hide questions section (from instructor)
+  showQuestions();
+  // start timer & show starting time (from instructor)
+  createTimer();
+}
+// Penalize function
+function penalizeTime() {
+  //   // display new time on page (from instructor)
+  timerEl.innerHTML = timerEl.innerHTML - 10
+  // if time, add in flash of red background color when penalized
+}
 function getQuestion() {
   // get current question object from array (from instructor)
-  questions[currentQuestionIndex];
+  var currentQuestion = questions[currentQuestionIndex];
   // update title with current question (from instructor)
-
+  questionTitle.innerHTML = currentQuestion.title;
   // clear out any old question choices (from instructor)
-
+  // currentQuestion[i-1].choices.classList.add("hide");
   // loop over choices (from instructor)
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
 
-  // create new button for each choice (from instructor)
+    // create new button for each choice (from instructor)
+    var newButton = document.createElement("button");
+    newButton.id = i;
+    newButton.innerHTML = +[i] + 1 + ": " + currentQuestion.choices[i];
+    newButton.className = "choices";
+    // display on the page (from instructor)
+    wrapper.appendChild(newButton);
+    newButton.onclick = checkAnswer;
+    // console.log(questions[currentQuestionIndex].answer);
+  }
 
-  // attach click event listener to each choice (from instructor)
-
-  // display on the page (from instructor)
+  function checkAnswer() {
+    // console.log(this.innerHTML[0]-1);
+    // console.log(currentQuestion.choices[parseInt(userSelection)])
+    var userSelection = this.innerHTML[0] - 1;
+    if (currentQuestion.choices[parseInt(userSelection)] == currentQuestion.answer) {
+      //   // play "right" sound effect (from instructor)
+      sfxRight.play();
+      $("#correct").show();
+    }
+    //   // else  (from instructor)
+    else {
+      //   // penalize time (from instructor)
+      penalizeTime();
+      //   // play "wrong" sound effect (from instructor)
+      sfxWrong.play();
+      $("#incorrect").show();
+    }
+  }
 }
-
-function questionClick() {
-  // check if user guessed wrong (from instructor)
-  // penalize time (from instructor)
-
-  // display new time on page (from instructor)
-
-  // play "wrong" sound effect (from instructor)
-
-  // else  (from instructor)
-  // play "right" sound effect (from instructor)
 
 
   // flash right/wrong feedback on page for half a second (from instructor)
 
-  // move to next question (from instructor)
+//   // move to next question (from instructor)
 
-  // check if we've run out of questions (from instructor)
-  // quizEnd (from instructor)
-  // else  (from instructor)
-  // getQuestion (from instructor)
-}
+//   // check if we've run out of questions (from instructor)
+//   // quizEnd (from instructor)
+//   // else  (from instructor)
+//   // getQuestion (from instructor)
+//   currentQuestionIndex++;
+//   getQuestion();
+// }
 
 function quizEnd() {
   // stop timer (from instructor)
@@ -118,9 +176,8 @@ function checkForEnter(event) {
 }
 
 // user clicks button to submit initials (from instructor)
-// submitBtn.onclick = saveHighscore;
+submitBtn.onclick = saveHighscore;
 
 // user clicks button to start quiz (from instructor)
 startBtn.addEventListener('click', startQuiz);
-
-// initialsEl.onkeyup = checkForEnter;
+initialsEl.onkeyup = checkForEnter;
